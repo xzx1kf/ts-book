94e1d03c0bbfa701d9d11703d60dd6183918faa4
xzx1kf <nicholas.hale@gmail.com>
2018-07-25 15:16:16 +0100

    Update to parse values in request for submission

--- CourtBookingPage.go
+++ CourtBookingPage.go
@@ -25,19 +25,20 @@ func BookCourt(w http.ResponseWriter, r *http.Request) {
 	v.Add("booking[booking_number]", "1")
 	v.Add("booking[full_name]", "Nick Hale")
 	v.Add("booking[membership_number]", "s119")
-	v.Add("booking[start_time]", "yyyy-MM-dd HH:mm:ss zzz")
+	v.Add("booking[start_time]", "yyyy-MM-dd HH:mm:ss zzz") // TODO
 	v.Add("booking[time_slot_id]", timeslot)
 	v.Add("booking[court_time]", "40")
 	v.Add("booking[court_id]", court)
-	v.Add("booking[days]", "21")
+	v.Add("booking[days]", "21") // TODO
 	v.Add("commit", "Book Court")
 
-	resp, err := http.PostForm("http://tynemouth-squash.herokuapp.com/bookings", v)
+	//resp, err := http.PostForm("http://tynemouth-squash.herokuapp.com/bookings", v)
 
+	/*
 	if err != nill {
 		log.Fatal(err)
 	}
-
+	*/
 	fmt.Println("Success")
 }
 
@@ -67,7 +68,17 @@ func ParseCourtBookingPage(doc goquery.Document) {
 	s.Find("input").Each(func(i int, sel *goquery.Selection) {
 		input, exists := sel.Attr("authenticity_token")
 		if exists {
-			fmt.Println("AT=" + input.Test())
+			fmt.Println("AT=" + input.Text())
+		}
+		
+		input, exists := sel.Attr("booking_start_time")
+		if exists {
+			fmt.Println("start time=" + input.Text())
+		}
+		
+		input, exists := sel.Attr("booking_days")
+		if exists {
+			fmt.Println("days=" + input.Text())
 		}
 	})
 }
