9913dd2b5ffc8a4dbbb19b2cb053f6ac03eaf66a
Nick Hale <nicholas.hale@gmail.com>
2018-07-30 22:47:04 +0100

    Fixed parsing of booking page

--- handlers.go
+++ handlers.go
@@ -11,41 +11,44 @@ import (
 
 func BookCourt(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
+	days := q.Get("days")
 	court := q.Get("court")
 	hour := q.Get("hour")
 	min := q.Get("min")
 	timeslot := q.Get("timeSlot")
 
-	doc, _ := GetCourtBookingPage(court, hour, min, timeslot)
+	doc, _ := GetCourtBookingPage(days, court, hour, min, timeslot)
 
-	token, days := ParseCourtBookingPage(doc)
+	token, time := ParseCourtBookingPage(doc)
 
 	v := url.Values{}
 	v.Set("authenticity_token", token)
 	v.Add("booking[booking_number]", "1")
 	v.Add("booking[full_name]", "Nick Hale")
 	v.Add("booking[membership_number]", "s119")
-	v.Add("booking[start_time]", "yyyy-MM-dd HH:mm:ss zzz") // TODO
+	v.Add("booking[start_time]", time)
 	v.Add("booking[time_slot_id]", timeslot)
 	v.Add("booking[court_time]", "40")
 	v.Add("booking[court_id]", court)
-	v.Add("booking[days]", days) // TODO
+	v.Add("booking[days]", days)
+	v.Add("utf8", "&#x2713;")
 	v.Add("commit", "Book Court")
 
-	//resp, err := http.PostForm("http://tynemouth-squash.herokuapp.com/bookings", v)
+	fmt.Println(v)
+	resp, err := http.PostForm("http://tynemouth-squash.herokuapp.com/bookings", v)
 
-	/*
-	if err != nill {
+	if err != nil {
 		log.Fatal(err)
 	}
-	*/
-	fmt.Println(v)
+
+	fmt.Println(resp.Status)
 	fmt.Println("Success")
 }
 
-func GetCourtBookingPage(court string, hour string, min string, timeSlot string) (*goquery.Document, error) {
+func GetCourtBookingPage(days string, court string, hour string, min string, timeSlot string) (*goquery.Document, error) {
 	res, err := http.Get("http://tynemouth-squash.herokuapp.com/bookings/new?" +
 		"court=" + court +
+		"&days=" + days +
 		"&hour=" + hour +
 		"&min=" + min +
 		"&timeSlot=" + timeSlot)
@@ -63,34 +66,33 @@ func GetCourtBookingPage(court string, hour string, min string, timeSlot string)
 	return doc, err
 }
 
-func ParseCourtBookingPage(doc *goquery.Document) (token string, days string) {
-	s := doc.Find("form.booking")
+func ParseCourtBookingPage(doc *goquery.Document) (token string, time string) {
+	s1 := doc.Find("input#booking_start_time")
+	s2, exist := s1.Attr("value")
+	if exist {
+		fmt.Println("Nick " +  s2)
+	} else {
+		fmt.Println("boo")
+	}
 
+	s := doc.Find("form.booking")
 	s.Find("input").Each(func(i int, sel *goquery.Selection) {
-		fmt.Println(sel)
 		input, exists := sel.Attr("name")
 		if (input == "authenticity_token") {
 			token, exists = sel.Attr("value")
 			fmt.Println(token)
-		} else if (input == "booking[days]") {
-			days, _ = sel.Attr("value")
-			fmt.Println("days: " + days)
-		}
-
-		input, exists = sel.Attr("booking_start_time")
-		if exists {
-			fmt.Println("start time=" + input)
-		}
-
-		input, exists = sel.Attr("booking_days")
-		if exists {
-			fmt.Println("days=" + input)
+		} else if (input == "booking[start_time]") {
+			time, exists = sel.Attr("value")
+			if exists {
+				fmt.Println("days: " + time)
+			} else {
+				fmt.Println("BOOK")
+			}
 		}
 	})
 
-	s.Find("input#booking_days")
 
-	return token, days
+	return token, time
 }
 
 
